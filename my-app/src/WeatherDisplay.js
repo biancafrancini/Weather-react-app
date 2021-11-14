37797dac5db674cc641f6640d2785d3a0e0eaa82
biancafrancini <francinibianca@gmail.com>
2021-11-14 08:53:55 +0000

    Added axios

--- my-app/src/WeatherDisplay.js
+++ my-app/src/WeatherDisplay.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Form from "./Form";
 import WeatherDetails from "./WeatherDetails";
+import axios from "axios";
 
 import "./styles.css";
 
 export default function WeatherDisplay() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  const apiKey = "15311e86ae668422281f7a4353f9243b";
+  let city = "Paris";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="container-md displayWeatherCity">
       <Form />
